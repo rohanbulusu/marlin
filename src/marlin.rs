a0fb23c34468b5309c3b00dbabd188098c56035f
Theoden <rohanbulusu@gmail.com>
2023-02-11 18:55:24 -0700

    removed unnecessary comments

--- src/marlin.rs
+++ src/marlin.rs
@@ -130,35 +130,32 @@ fn generate_render_pipeline(gpu: &wgpu::Device, config: &wgpu::SurfaceConfigurat
         layout: Some(layout),
         vertex: wgpu::VertexState {
             module: shader,
-            entry_point: "vert_main", // 1.
-            buffers: &[Vertex::desc()], // 2.
+            entry_point: "vert_main",
+            buffers: &[Vertex::desc()],
         },
-        fragment: Some(wgpu::FragmentState { // 3.
+        fragment: Some(wgpu::FragmentState {
             module: &shader,
             entry_point: "frag_main",
-            targets: &[Some(wgpu::ColorTargetState { // 4.
+            targets: &[Some(wgpu::ColorTargetState {
                 format: config.format,
                 blend: Some(wgpu::BlendState::REPLACE),
                 write_mask: wgpu::ColorWrites::ALL,
             })],
         }),
         primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+            topology: wgpu::PrimitiveTopology::TriangleList,
             strip_index_format: None,
-            front_face: wgpu::FrontFace::Ccw, // 2.
+            front_face: wgpu::FrontFace::Ccw,
             cull_mode: Some(wgpu::Face::Back),
-            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
             polygon_mode: wgpu::PolygonMode::Fill,
-            // Requires Features::DEPTH_CLIP_CONTROL
             unclipped_depth: false,
-            // Requires Features::CONSERVATIVE_RASTERIZATION
             conservative: false,
         },
-        depth_stencil: None, // 1.
+        depth_stencil: None,
         multisample: wgpu::MultisampleState {
-            count: 1, // 2.
-            mask: !0, // 3.
-            alpha_to_coverage_enabled: false, // 4.
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
         },
         multiview: None, // 5.
     })
