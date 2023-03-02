09355988d5045832bc29a37b04c6639bce9e4937
Rohan <Rohan@MacBook-Pro.local>
2023-03-01 20:08:05 -0700

    fixed formatting

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 mod marlin;
 
-use marlin::{Entity, Vertex};
-
 #[tokio::main]
 async fn main() {
-    let tri = Entity::from_points(vec![
-        Vertex::new(0.0, 0.5, 0.0, [1.0, 0.0, 0.0]),
-        Vertex::new(-0.5, -0.5, 0.0, [0.0, 1.0, 0.0]),
-        Vertex::new(0.5, -0.5, 0.0, [0.0, 0.0, 1.0]),
-    ]);
-
-    marlin::run(vec![tri]).await;
+    marlin::run().await;
 }
