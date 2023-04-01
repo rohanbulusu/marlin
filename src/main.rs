7649453a232d79c8ba1da2e848a58fb2242c40e6
Theoden <rohanbulusu@gmail.com>
2023-04-01 15:31:49 -0700

    fixed rendering dimension bug

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ async fn main() {
 
     let mut window = Marlin::new(400.0, 400.0).await;
     window.state().window().set_title("Marlin");
-    println!("({}, {})", window.state().window().inner_size().width, window.state().window().inner_size().height);
-
+    
     let tri = Entity::from_points(&window, vec![
         Point::new(200.0, 100.0, [1.0, 0.0, 0.0]),
         Point::new(100.0, 200.0, [1.0, 0.0, 0.0]),
